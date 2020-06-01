069a2c0dfd054c8d3601e0d591d9fa62720179af
vitaliypodlypnyi <vitaliy.podlipnyi@zwapgrid.com>
2020-06-01 19:39:14 +0300

    Add iframe message handling (integration creation) [#168661241]

--- CredentialPostTest/CredentialPostTest/wwwroot/js/site.js
+++ CredentialPostTest/CredentialPostTest/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function bindEvent(element, eventName, eventHandler) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, eventHandler, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, eventHandler);
+    }
+}
+
+//here goes the code which should be run on iframe parent when integration is created 
+function processResult(result) {
+    let systems = result.split('|');
+    let sourceSystem = systems[0];
+    let targetSystem = systems[1];
+    var message = document.getElementById('message');
+    if (message) {
+        message.removeAttribute("hidden");
+        message.innerHTML = 'Integration between <b>' + sourceSystem + '</b> and <b>' + targetSystem + '</b> was created successfully!';
+    }
+}
+
+bindEvent(window, 'message', function (e) {
+    processResult(e.data);
+});
